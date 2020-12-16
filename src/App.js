477663a01f7382b8b0eefb2d998980c913fcda07
kurupong02 <kurupongwanpen1@gmail.com>
2020-12-16 08:48:55 +0700

    update insert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import forEach from 'lodash/forEach'
 import split from 'lodash/split'
 import includes from 'lodash/includes'
 import FileSaver from 'file-saver'
+
+import { data } from './data'
 
 const underscoreToCamelcase = (v) => {
   return v.toLowerCase().replace(/_([a-z0-9])/g, function (g) { return g[1].toUpperCase(); });
@@ -18,7 +20,7 @@ const textbox = (list) => {
   var dto1 = ''
   var dto2 = ''
   var type = ''
-
+  var model = ''
   forEach(list, (v, index) => {
     const set = underscoreToCamelcase(`set_${v.name}`)
     const get = underscoreToCamelcase(`get_${v.type}`)
@@ -27,6 +29,7 @@ const textbox = (list) => {
     selectRow += `${v.name} AS ${underscoreToCamelcase(v.name)},\n`
     name += `${v.name}`
     value = `${value}model.${underscoreToCamelcase(`get_${v.name}`)}()`
+    model = `${model}model.${underscoreToCamelcase(`get_${v.name}`)}()`
     insertValue = `${insertValue}?`
     updateText += `${v.name} = ?,\n`
     dto1 += `dto.${set}(${index + 1}, ${underscoreToCamelcase(v.name)});\n`
@@ -36,11 +39,12 @@ const textbox = (list) => {
       name = `${name}, `
       insertValue = `${insertValue}, `
       value = `${value} +","+ `
+      model = `${model}, `
     }
   })
 
   const line = "\n=================\n\n"
-  const result = `${selectRow}${line}${updateText}${line}${`(${name}) VALUES (${value});\n`}${line}${`(${name}) value (${insertValue});\n`}${line}${dto1}${line}${dto2}${line}${type}`
+  const result = `${selectRow}${line}${updateText}${line}${`(${name}) VALUES (${value});\n`}${line}${`(${name}) value (${insertValue});\n`}${line}{${model}};\n${line}${dto1}${line}${dto2}${line}${type}`
 
   return (
     <textarea id="textbox" value={result}>Type something here</textarea>
@@ -70,6 +74,57 @@ function App() {
   const [fileName, setFileName] = useState('')
   const [text, setText] = useState('')
 
+  useEffect(() => {
+    const name = async (params) => {
+      forEach(data, async (v, i) => {
+        const text = v.data
+        const valueReplace = text.replace(/\t/g, "").replace(/\n/g, "").replace(/" /g, ";").replace(/ /g, "").replace(/"/g, "")
+        const valueSplit = split(valueReplace, ',')
+
+        const mm = valueSplit.map((v) => {
+          const tSplit = split(v, ';')
+          return { name: tSplit[0], type: getType(tSplit[1]) }
+        })
+
+        var selectRow = ''
+        var name = ''
+        var value = ''
+        var insertValue = ''
+        var updateText = ''
+        var dto1 = ''
+        var dto2 = ''
+        var type = ''
+        var model = ''
+        forEach(mm, (v, index) => {
+          const set = underscoreToCamelcase(`set_${v.name}`)
+          const get = underscoreToCamelcase(`get_${v.type}`)
+          selectRow += `${v.name} AS ${underscoreToCamelcase(v.name)},\n`
+          name += `${v.name}`
+          value = `${value}model.${underscoreToCamelcase(`get_${v.name}`)}()`
+          model = `${model}model.${underscoreToCamelcase(`get_${v.name}`)}()`
+          insertValue = `${insertValue}?`
+          updateText += `${v.name} = ?,\n`
+          dto1 += `dto.${set}(${index + 1}, ${underscoreToCamelcase(v.name)});\n`
+          dto2 += `dto.${set}(rs.${get}("${underscoreToCamelcase(v.name)}"));\n`
+          type += `private ${v.type} ${underscoreToCamelcase(v.name)};\n`
+          if (index + 1 !== mm.length) {
+            name = `${name}, `
+            insertValue = `${insertValue}, `
+            value = `${value} +","+ `
+            model = `${model}, `
+          }
+        })
+
+        const line = "\n=================\n\n"
+        const result = `${selectRow}${line}${updateText}${line}${`(${name}) VALUES (${value});\n`}${line}${`(${name}) value (${insertValue});\n`}${line}{${model}};\n${line}${dto1}${line}${dto2}${line}${type}`
+        console.log(result)
+
+        const file = await makeTextFile(result)
+        await FileSaver.saveAs(file, `${v.name}.txt`);
+      })
+    }
+    name()
+  }, [])
 
   const getType = (value) => {
     if (includes(value, 'NUMBER')) return 'BigDecimal'
@@ -87,8 +142,6 @@ function App() {
 
     const data = valueSplit.map((v) => {
       const t = v.replace(/" /g, ';').replace(/"/g, '')
-      console.log(t)
-
       const tSplit = split(t, ';')
       return { name: tSplit[0], type: getType(tSplit[1]) }
     })
@@ -98,7 +151,8 @@ function App() {
 
   return (
     <div className="container">
-      <div className="container-left">
+      <h2>{data.length}</h2>
+      {/* <div className="container-left">
         <br />
         <br />
         <textarea value={text} onChange={handleOnChangeText}></textarea>
@@ -110,7 +164,7 @@ function App() {
           <button onClick={() => saveFile(fileName)}>save</button>
         </div>
         {textbox(list)}
-      </div>
+      </div> */}
     </div>
   );
 }
